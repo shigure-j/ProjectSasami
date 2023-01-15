650545a4fe212dbb99a5af2bfb905862fdb6fdbe
j.shigure <j@shigure.org>
2023-01-15 18:43:00 +0800

    Add license, clean file

--- test_input/format/import.rb
+++
@@ -1,8 +0,0 @@
-%w(project design owner work).each do |type|
-  fmt = Format.find_by(name: type)
-  if fmt.nil?
-    fmt = Format.new(name: type)
-  end
-  fmt.format = File.read("test_input/format/#{type}.json")
-  fmt.save
-end
